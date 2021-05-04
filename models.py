5ce525a36cda9369b1a58d7491053a81ed3771ad
Justin <jforseth210@gmail.com>
2021-05-03 18:58:41 -0600

    Basic transaction model

--- models.py
+++ models.py
@@ -48,16 +48,19 @@ class Cow(db.Model):
                 return event.date
 
     def search(self, query, tags=[], sexes=[], owners=[], sires=[], dams=[]):
-        search_match = query.lower() in repr(self).lower() 
+        search_match = query.lower() in repr(self).lower()
         tag_match = not tags or self.get_first_digit_of_tag() in tags
         sex_match = not sexes or self.sex in sexes
         owner_match = not owners or self.owner in owners
-        sire_match = not sires or (self.get_sire() is not None and self.get_sire().tag_number in sires)
-        dam_match = not dams or (self.get_dam() is not None and self.get_dam().tag_number in dams)
+        sire_match = not sires or (
+            self.get_sire() is not None and self.get_sire().tag_number in sires)
+        dam_match = not dams or (
+            self.get_dam() is not None and self.get_dam().tag_number in dams)
         return search_match and tag_match and sex_match and owner_match and sire_match and dam_match
 
-    def toSearchResult(self,query):
+    def toSearchResult(self, query):
         return SearchResult(self.tag_number, repr(self).replace(query, f"<b>{query}</b>"), f"/cow/{self.tag_number}")
+
     def get_first_digit_of_tag(self):
         first_digit = re.search("\d", self.tag_number)
         first_digit = first_digit.group()[0] if first_digit else "N/A"
@@ -81,11 +84,28 @@ class Event(db.Model):
         date_match = not dates or self.date in dates
         name_match = not names or self.name in names
         return query_match and date_match and name_match
+
     def toSearchResult(self, query):
         return SearchResult(self.name, repr(self).replace(query, f"<b>{query}</b>"), f"/event/{self.event_id}")
 
     def __repr__(self):
         return f"Event on {self.date}: {self.name} - {self.description}"
+
+
+class Transaction(db.Model):
+    transaction_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    amount = db.Column(db.Float)
+    description = db.Column(db.Text)
+
+    event_id = db.Column('event_id', db.Integer, db.ForeignKey(
+        'event.event_id'), nullable=False)
+
+    event = db.relationship(
+        'Event', backref=db.backref('transactions', lazy=True))
+
+    def __repr__(self):
+        return f"Transaction {self.transaction_id}: {self.amount} - {self.name} - {self.description}"
 
 
 class SearchResult():
