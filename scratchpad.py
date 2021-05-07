5a0e9997d3da27acb9661a29eefd9c831e75220f
Justin <jforseth210@gmail.com>
2021-05-07 11:53:34 -0600

    Delete scratchpad

--- scratchpad.py
+++
@@ -1,5 +0,0 @@
-from main import *
-cow = Cow.query.filter_by(tag_number='814').first()
-cow.events.append(Event(date="2018-01-01", name="Born", description="Born"))
-cow.events.append(Event(date="2018-06-02", name="Branded", description="Branded"))
-db.session.commit()
